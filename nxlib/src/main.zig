2130162e537278554f7eb4a969c38eef49e70399
Koleman Nix <kolemannix@gmail.com>
2023-10-09 21:51:19 -0400

    Implement char to string in zig as PoC

--- nxlib/src/main.zig
+++ nxlib/src/main.zig
@@ -1,8 +1,11 @@
-const NexString = packed struct {
+const std = @import("std");
+const allocator = std.heap.c_allocator;
+
+const NxString = packed struct {
     len: u64,
     data: [*]u8,
 };
-fn NexArray(T: type) type {
+fn NxArray(T: type) type {
     return struct {
         len: u64,
         data: [*]T,
@@ -10,10 +13,14 @@ fn NexArray(T: type) type {
 }
 
 export const HELLO_WORLD = "Hello, World";
+// const s = NxString{ .len = 4, .data = [4]u8{ 'a', 's', 'd', 'f' } };
 
-export fn zigadd(a: i64, b: i64) i64 {
-    const s = NexString{ .len = 4, .data = [4]u8{ 'a', 's', 'd', 'f' } };
-    return a + b;
+export fn _nx_charToString(c: u8) *NxString {
+    const new_str: *NxString = allocator.create(NxString) catch unreachable;
+    const data = allocator.alloc(u8, 1) catch unreachable;
+    data[0] = c;
+    new_str.* = .{ .len = 1, .data = data.ptr };
+    return new_str;
 }
 
 // const std = @import("std");
