d928c63ba9938031bfbeaada102bb3e065609cc3
Koleman Nix <kolemannix@gmail.com>
2025-01-22 22:16:58 -0500

    Knee-deep in it; sret working, almost there, need a 'smart store'

--- agg.c
+++ agg.c
@@ -6,10 +6,19 @@ long one(struct P3 p) {
   return p.x;
 }
 
+struct P3 structRet() {
+  struct P3 p;
+  p.x = 1;
+  p.y = 2;
+  p.z = 3;
+  return p;
+}
+
 int main() {
   struct P3 p;
   p.x = 3;
   p.y = 4;
   p.z = 0;
-  return (int)one(p);
+  struct P3 p3 = structRet();
+  return (int)p3.z;
 }
