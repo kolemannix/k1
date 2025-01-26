301aaf3e4068715fcb65d61d523643b6116eb7da
Koleman Nix <kolemannix@gmail.com>
2025-01-25 21:57:25 -0500

    Make refchess the 'dogfood' project for test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ export RUST_BACKTRACE=1
 cargo test
 cargo run --bin test_suite -- $1
 
-./run.sh dogfood/a_proj
+./run.sh dogfood/refchess
 
 if rg --type-add 'k1:*.k1' -c 'nocommit' -t rust -t c -t k1 .
 then
