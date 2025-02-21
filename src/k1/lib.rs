44effb41e9299005d4f9533ed67fccb6294941c9
Koleman Nix <kolemannix@gmail.com>
2025-02-20 22:12:41 -0500

    expr pool x4, compiling

--- src/k1/lib.rs
+++ src/k1/lib.rs
@@ -9,4 +9,4 @@ pub mod typer;
 static_assert_size!(parse::ParsedStmt, 24); // Get down below 100 // We did it!
 static_assert_size!(parse::ParsedExpression, 120); // Get back down ideally below 50
 static_assert_size!(typer::TypedExpr, 72);
-static_assert_size!(typer::TypedStmt, 16);
+static_assert_size!(typer::TypedStmt, 20);
