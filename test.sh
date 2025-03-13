dcf0fd50be3590eec8eef8b1b56958d4f6721a2b
Koleman Nix <kolemannix@gmail.com>
2025-03-12 20:11:51 -0400

    Make test.sh cargo build fewer times

--- test.sh
+++ test.sh
@@ -4,10 +4,15 @@ set -x
 # Exit on errors
 set -e
 export RUST_BACKTRACE=1
+cargo build --features=llvm-sys/prefer-dynamic --bin test_suite
+cargo build --features=llvm-sys/prefer-dynamic --bin compiler
 cargo test
-cargo run --features=llvm-sys/prefer-dynamic --bin test_suite -- $1
 
-./check.sh dogfood/refchess
+
+target/debug/test_suite $1
+
+export RUST_LOG=info
+target/debug/compiler --write-llvm check dogfood/refchess
 
 if rg --type-add 'k1:*.k1' -c 'nocommit' -t rust -t c -t k1 .
 then
