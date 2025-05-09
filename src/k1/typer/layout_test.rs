cbc118a178766d7da715cf326ea3bd15e2b5f1b3
Koleman Nix <kolemannix@gmail.com>
2025-05-08 21:00:17 -0400

    Convert Layout to bytes not bits

--- src/k1/typer/layout_test.rs
+++ src/k1/typer/layout_test.rs
@@ -4,28 +4,25 @@ use crate::typer::*;
 fn struct_layout_1() {
     let mut l = Layout::ZERO;
     l.append_to_aggregate(Layout::from_scalar_bits(64));
-    assert_eq!(l.size_bits, 64);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 64);
-    assert_eq!(l.size_bytes(), 8);
-    assert_eq!(l.align_bytes(), 8);
-    assert_eq!(l.stride_bytes(), 8);
+    assert_eq!(l.size, 8);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 8);
+
+    assert_eq!(l.size_bits(), 64);
+    assert_eq!(l.align_bits(), 64);
 
     l.append_to_aggregate(Layout::from_scalar_bits(8));
-    assert_eq!(l.size_bits, 72);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 128);
-    assert_eq!(l.size_bytes(), 9);
-    assert_eq!(l.align_bytes(), 8);
-    assert_eq!(l.stride_bytes(), 16);
+    assert_eq!(l.size, 9);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 16);
+
+    assert_eq!(l.size_bits(), 72);
+    assert_eq!(l.align_bits(), 64);
 
     l.append_to_aggregate(Layout::from_scalar_bits(32));
-    assert_eq!(l.size_bits, 128);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 128);
-    assert_eq!(l.size_bytes(), 16);
-    assert_eq!(l.align_bytes(), 8);
-    assert_eq!(l.stride_bytes(), 16);
+    assert_eq!(l.size, 16);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 16);
 }
 
 #[test]
@@ -33,31 +30,31 @@ fn add_zero_no_change() {
     let mut l = Layout::ZERO;
     let o1 = l.append_to_aggregate(Layout::from_scalar_bits(64));
     assert_eq!(o1, 0);
-    assert_eq!(l.size_bits, 64);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 64);
+    assert_eq!(l.size, 8);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 8);
     let o2 = l.append_to_aggregate(Layout::from_scalar_bits(8));
-    assert_eq!(o2, 64);
-    assert_eq!(l.size_bits, 72);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 128);
+    assert_eq!(o2, 8);
+    assert_eq!(l.size, 9);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 16);
     let o3 = l.append_to_aggregate(Layout::ZERO);
-    assert_eq!(o3, 72);
-    assert_eq!(l.size_bits, 72);
-    assert_eq!(l.align_bits, 64);
-    assert_eq!(l.stride_bits(), 128);
+    assert_eq!(o3, 9);
+    assert_eq!(l.size, 9);
+    assert_eq!(l.align, 8);
+    assert_eq!(l.stride(), 16);
 }
 
 #[test]
 fn array_me() {
     let l = Layout::ZERO.array_me(10);
-    assert_eq!(l.size_bits, 0);
-    assert_eq!(l.align_bits, 8);
-    assert_eq!(l.stride_bits(), 0);
-    let l2 = Layout { size_bits: 40, align_bits: 32 };
-    assert_eq!(l2.stride_bits(), 64);
+    assert_eq!(l.size, 0);
+    assert_eq!(l.align, 1);
+    assert_eq!(l.stride(), 0);
+    let l2 = Layout { size: 5, align: 4 };
+    assert_eq!(l2.stride(), 8);
     let l3 = l2.array_me(10);
-    assert_eq!(l3.size_bits, 640);
-    assert_eq!(l3.align_bits, 32);
-    assert_eq!(l3.stride_bits(), 640);
+    assert_eq!(l3.size, 80);
+    assert_eq!(l3.align, 4);
+    assert_eq!(l3.stride(), 80);
 }
