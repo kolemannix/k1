8f0815db801c378f2adb324e392bbfef77774765
Koleman Nix <kolemannix@gmail.com>
2025-12-01 06:34:46 -0500

    callconv helper c file

--- llvmc.sh
+++ llvmc.sh
@@ -3,5 +3,6 @@ set -e
 set -x
 FPATH=$1
 outpath=$(dirname $FPATH)/$(basename $FPATH .c).ll
-cc -S -emit-llvm -O0 $FPATH -o $outpath 
+# cc -S -emit-llvm -O0 $FPATH -o $outpath 
+cc -target x86_64-unknown-linux -S -emit-llvm -O0 $FPATH -o $outpath 
 cat $outpath | less
