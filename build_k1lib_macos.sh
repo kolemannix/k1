60a2a7429716ee4bd6cab0ff3542d5f2a61df85b
William Lagos <wlagos@patterndata.ai>
2025-12-05 11:03:46 -0500

    use macos command line sdk instead of xcode sdk

--- build_k1lib_macos.sh
+++ build_k1lib_macos.sh
@@ -2,9 +2,9 @@ set -e
 set -x
 cd k1lib
 
-$LLVM_SYS_180_PREFIX/bin/clang -mmacosx-version-min=15.0 --sysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
+$LLVM_SYS_180_PREFIX/bin/clang -mmacosx-version-min=15.0 --sysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
   -flto -c k1rt.c
 ar rcs libk1rt.a k1rt.o
 
-$LLVM_SYS_180_PREFIX/bin/clang -mmacosx-version-min=15.0 --sysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
+$LLVM_SYS_180_PREFIX/bin/clang -mmacosx-version-min=15.0 --sysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
   -fPIC -shared k1rt.c -o libk1rt.dylib
