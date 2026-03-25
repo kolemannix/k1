337bf0864dc238c9ab1e80c0f620bf731bb9ea30
Koleman Nix <kolemannix@gmail.com>
2026-03-24 23:45:42 -0400

    modules_test: platform independent build script

--- test_src/modules_test/build.sh
+++ test_src/modules_test/build.sh
@@ -4,4 +4,9 @@ cd libs
 clang -c foo.c -o foo.o
 ar rcs libfoo.a foo.o
 
-clang -fPIC -shared foo.c -o libfoo.dylib
+case "$(uname -s)" in
+  Darwin) SHLIB_EXT="dylib" ;;
+  *)      SHLIB_EXT="so"    ;;
+esac
+
+clang -fPIC -shared foo.c -o libfoo.$SHLIB_EXT
