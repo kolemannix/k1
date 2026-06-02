318a5db54f1d439dbb5fe50fc169b23d55f8eedf
Koleman Nix <kolemannix@gmail.com>
2026-06-02 09:02:42 -0400

    wip bindgen on linux

---
+++ dogfood/bindgen.k1/io_uring/io_uring_bindings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <liburing.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+	return 0;
+}
