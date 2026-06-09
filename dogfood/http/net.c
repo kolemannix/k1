c8b7d33b27f03b5747e90d06f50fbff3f45b1fd7
Koleman Nix <kolemannix@gmail.com>
2026-06-09 16:19:55 -0400

    towards compiling gen'd libuv

---
+++ dogfood/http/net.c
@@ -0,0 +1,4 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
--- dogfood/http/pthread.c
+++
@@ -1,8 +0,0 @@
-#include <pthread.h>
-#include <sys/_types/_pid_t.h>
-
-int main() {
-  pid_t asdf;
-  pthread_once_t a;
-  return 0;
-}
